b5dfe4e3c277961a17769cef148c3e1b2befaf9e
Ronald-Torrens <rtacosta@hotmail.com>
2026-04-03 21:21:58 +0200

    Remove duplicate nickname constraint

--- api/db/migrations/20260403191206-remove-duplicate-nickname-index.js
+++ api/db/migrations/20260403191206-remove-duplicate-nickname-index.js
@@ -3,13 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeIndex('users', 'users_nickname_key1');
+    await queryInterface.removeConstraint('users', 'users_nickname_key1');
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.addIndex('users', ['nickname'], {
-      name: 'users_nickname_key1',
-      unique: true
+    await queryInterface.addConstraint('users', {
+      fields: ['nickname'],
+      type: 'unique',
+      name: 'users_nickname_key1'
     });
   }
 };
